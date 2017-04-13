11951cd203e3474037f854dc8a3aeb21aa81613f
Yuliya Kaloshych <Yuliya_Kaloshych@epam.com>
2017-04-13 17:08:07 +0300

    build with grunt

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ electron + react first app
 
 
 let get started!
+npm install -g gulp-cli
+
+
--- app/main.js
+++ app/main.js
@@ -9,24 +9,20 @@ app.on('ready', function() {
     show: false
   }); //appWindow
 
-  appWindow.loadURL('http://raybo.org');
+  appWindow.loadURL('file://' + __dirname + '/index.html');
 
   infoWindow = new BrowserWindow({
     width: 400,
     height: 300,
     transparent: true,
     show: false// ,
-    //frame: false
+    // frame: false
   }); //infoWindow
 
   infoWindow.loadURL('file://' + __dirname + '/info.html');
 
   appWindow.once('ready-to-show', function() {
     appWindow.show();
-    setTimeout(function() {
-      infoWindow.show();
-      //setTimeout(function() { infoWindow.hide();}, 3000);
-    }, 1000);
   }); //ready-to-show
 
   ipc.on('closeInfoWindow', function(event, arg){
