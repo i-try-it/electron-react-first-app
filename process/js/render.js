60a430376ae2904545af23db98a1ca3e6b8deae5
i_try_it <Yuliya_Kaloshych@epam.com>
2017-04-14 10:46:54 +0300

    creating list component

--- process/js/render.js
+++ process/js/render.js
@@ -1,55 +1,45 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-
-
-
-
-
-
-
 var $ = jQuery = require('jquery');
 var bootstrap = require('bootstrap');
-var fs=eRequire('fs');
+var fs = eRequire('fs');
 var loadApts = JSON.parse(fs.readFileSync(dataLocation));
+
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var AptList = require('./AptList');
 
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
       myAppointments: loadApts
-    }
-  },
-  render: function(){
+    }//return
+  }, //getInitialState
+  render: function() {
     var myAppointments = this.state.myAppointments;
+
+    myAppointments=myAppointments.map(function(item, index) {
+      return(
+        <AptList key = {index}
+          singleItem = {item}
+        />
+      ) // return
+    }.bind(this)); //Appointments.map
     return(
-            <div className="application">
-              <div className="container">
-              <div className="row">
-                <div className="appointments col-sm-12">
-                  <h2 className="appointments-headline">Current Appointments</h2>
-                  <ul className="item-list media-list">
-
-                    <li className="pet-item media">
-                      <div className="pet-info media-body">
-                        <div className="pet-head">
-                          <span className="pet-name">{myAppointments[0].petName}</span>
-                          <span className="apt-date pull-right">{myAppointments[0].aptDate}</span>
-                        </div>
-                        <div className="owner-name"><span className="label-item">Owner:</span>
-                        {myAppointments[0].ownerName}</div>
-                        <div className="apt-notes">{myAppointments[0].aptNotes}</div>
-                      </div>
-                    </li>
-
-                  </ul>
-                </div>{/* col-sm-12 */}
-              </div>{/* row */}
-              </div>{/* container */}
-            </div>
-    )
-  }
-})
+      <div className="application">
+        <div className="container">
+         <div className="row">
+           <div className="appointments col-sm-12">
+             <h2 className="appointments-headline">Current Appointments</h2>
+             <ul className="item-list media-list">{myAppointments}</ul>
+           </div>{/* col-sm-12 */}
+         </div>{/* row */}
+        </div>{/* container */}
+      </div>
+    );
+  } //render
+});//MainInterface
 
 ReactDOM.render(
   <MainInterface />,
   document.getElementById('petAppointments')
-)
+); //render
